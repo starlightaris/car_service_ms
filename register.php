4034df44c49605d72fae80af2ecf2fe797e5622b
bini-66 <binithi.vihanga@gmail.com>
2025-02-02 16:09:44 +0530

    commit-5

--- register.php
+++ register.php
@@ -1,3 +1,50 @@
+<?php
+
+
+include 'php/conf.php';
+$error_message = [];
+if(isset($_POST['submit'])){
+   
+
+$fname=$_POST['txtfname'];
+$lname=$_POST['txtlname'];
+$email=$_POST['txtemail'];
+$pw=$_POST['txtconpw'];
+$phone=$_POST['txttel'];
+
+$sql="SELECT userId FROM user WHERE  username='$email'";
+$result=mysqli_query($con,$sql);
+
+
+if (mysqli_num_rows($result) > 0) {
+    // Email already registered
+    $error_message[] = 'Email already registered!';
+
+}
+
+
+else{
+
+
+$sql1="INSERT INTO user (username,password) VALUES ('$email','$pw')";
+$result1=mysqli_query($con,$sql1);
+
+$userId = mysqli_insert_id($con);
+
+$sql2="INSERT INTO customer (firstName,lastName,email,phone,userId) VALUES ('$fname','$lname','$email','$phone','$userId')";
+$result2=mysqli_query($con,$sql2);
+
+
+if ($result1 === false || $result2 === false) {
+    echo "Error: " . mysqli_error($con);
+}
+
+header('location:login.php');
+
+}
+}
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -6,10 +53,10 @@
   
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="js/Validate(register).js"></script>
+        <script src="js/Validate-register.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="css/style(register).css">
+        <link rel="stylesheet" type="text/css" href="css/style-register.css">
     </head>
     <body>
         <div class="container">
@@ -22,16 +69,16 @@
                     Please enter a valid email address.
                 </div>
                 
-                <form name="myform" onsubmit="return doValidate()" method="POST" action="php/register.php">
+                <form name="myform" onsubmit="return doValidate()" method="POST" action="">
                     <div class="row g-3">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <input type="text" name="txtfname" class="form-control input-field" placeholder="First Name" required>
+                                <input type="text" name="txtfname" class="form-control input-field" placeholder="First Name" value="<?php echo isset($fname) ? $fname : ''; ?>" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <input type="text" name="txtlname" class="form-control input-field" placeholder="Last Name" required>
+                                <input type="text" name="txtlname" class="form-control input-field" placeholder="Last Name" value="<?php echo isset($lname) ? $lname : ''; ?>" required>
                             </div>
                         </div>
                     </div>
@@ -45,19 +92,23 @@
                     </div>
                     <div class="row g-3">
                         <small id="emailError" class="error-text"> Please enter a valid email address.</small>
-                        <?php 
-                        include 'register.php';
-                        if (isset($error_message) ) {
-                            echo    ' <small id="emailError" class="error-text">'.$error_message.'</small>';
-                        }
-                    ?>
-                    
+                       
+                       <?php 
+                  
+                  if (isset($error_message) ) {
+                    foreach($error_message as $error_message){
+                        echo '<div class="error-message">'.$error_message.'</div>';
+                  }
+                }
+              ?>
+
+                                                
                     </div>
             
                         <div class="row g-3">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="password" name="txtpw" class="form-control input-field" placeholder="Password" required>
+                                    <input type="password" name="txtpw" class="form-control input-field" placeholder="Password"  required>
                                 </div>
                             </div>
                        
@@ -74,7 +125,7 @@
                     <div class="row g-3">
                         <div class="col-12">
                             <div class="form-group">
-                                <input type="tel" name="txttel" class="form-control input-field" placeholder="Phone Number" required>
+                                <input type="tel" name="txttel" class="form-control input-field" placeholder="Phone Number" value="<?php echo isset($phone) ? $phone : ''; ?>" required>
                             </div>
                         </div>
                     </div>
@@ -84,7 +135,7 @@
                     <div class="row g-3">
                         <div class="col-12">
                             <div class="form-group">
-                                <input type="submit" class="btn btn-primary w-100 submit" name="btnsubmit" value="Register">
+                                <input type="submit" class="btn btn-primary w-100 submit" name="submit" value="Register">
                             </div>
                         </div>
                     </div>
